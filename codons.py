c5efd273eb3692279daa11777306a3876ca1b349
Netalerner852 <netalerner852@gmail.com>
2024-12-12 16:19:52 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -3,10 +3,9 @@ def create_codon_dict(file_path):
     rows = file.readlines()
     file.close()
     d = {}
- for row in rows:
+    for row in rows:
         cells = row.strip().split('\t')
         codon = cells[0]
         amino_acid = cells[2]
         d[codon] = amino_acid
     return d
-
